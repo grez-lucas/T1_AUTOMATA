9fb3f5bf15767af2c856a5e9eaed8df8bcf2e86a
grez-lucas <lgrez1@miuandes.cl>
2022-04-28 18:15:47 -0400

    feat(missing last step)

--- dfa_min/dfamin.py
+++ dfa_min/dfamin.py
@@ -7,6 +7,9 @@ state = r'[\d\w]+'
 symbol = r'[\d\w]'
 transition = rf'({state})\s*({symbol})\s*->\s*({state})'
 
+def order_tuple(a,b):
+			return (a,b) if a < b else (b,a)
+
 def regex_find_span(text,word):
      pattern = re.compile(word)
      match = pattern.search(text)
@@ -32,6 +35,9 @@ with open(os.path.join(__location__, file_name+".txt")) as file:
     original_dfa = { "s_state" : set() , "n_states" : set() , "e_states" : set()}
     minimized_dfa = { "s_state" : set() , "n_states" : set() , "e_states" : set()}
 
+    
+    states = set()
+
     alphabet = set()
 
     transitions = {}
@@ -53,12 +59,15 @@ with open(os.path.join(__location__, file_name+".txt")) as file:
     
         if row[0] == ">":
             original_dfa["s_state"].add(row[1:-1])
+            states.add(row[1:-1])
             continue
         elif row[0] == "*":
             original_dfa["e_states"].add(row[1:-1])
+            states.add(row[1:-1])
             continue
         else:
             original_dfa["n_states"].add(row[:-1])
+            states.add(row[:-1])
         
         row_counter+=1
     
@@ -92,17 +101,51 @@ with open(os.path.join(__location__, file_name+".txt")) as file:
         row_counter+=1
 
 
-    # minimization algorithm
-    
+    # ==== MINIMIZATION ALGORITHM ====
+    table = {}
+
+    #sort states for table creation
+    sorted_states = sorted(states)
+
+    #Step 1: initialize table with first checkmarks
+    for i,state in enumerate(sorted_states):
+        for state_2 in sorted_states[i + 1:]:
+            #XOR,only one can be final
+            table[state, state_2] = (state in original_dfa["e_states"]) ^ (state_2 in original_dfa["e_states"])
+
+    print(table)
+    #Step 2: Check unmarked values twice
+    flag = 1
+    while flag < 2:
+        for key1,key2 in table:
+            if table[(key1,key2)] == False:
+                for symbol in alphabet:
+                    state1 = transitions[frozenset({key1,symbol})]
+                    state2 = transitions[frozenset({key2,symbol})]
+                    if state1 != state2:
+                        if table[order_tuple(max(state1),max(state2))] == True:
+                            table[(key1,key2)] = True
+                            break
+        flag +=1
+
+    minimized_states = set()
+    for state1,state2 in table:
+        if table[(state1,state2)] == False:
+            minimized_states.add(frozenset({state1} | {state2}))
 
 
-    # print for testing
+
+    # prints for testing
     for key in original_dfa:
         print(key, original_dfa[key])
 
-    print(alphabet)
+    print("states", states)
+
+    print("alphabet", alphabet)
 
     for key in transitions:
         print(key, transitions[key])
 
     print(transitions[frozenset(['0','B'])])
+    print(table)
+    print(minimized_states)
