4a48a9a86c2451f118e8e7e048ca35038cac4a33
grez-lucas <lgrez1@miuandes.cl>
2022-04-28 16:13:00 -0400

    feat(input comprehension)

--- dfa_min/dfamin.py
+++ dfa_min/dfamin.py
@@ -3,6 +3,10 @@ import re
 
 file_name = "dfa_input"
 
+state = r'[\d\w]+'
+symbol = r'[\d\w]'
+transition = rf'({state})\s*({symbol})\s*->\s*({state})'
+
 def regex_find_span(text,word):
      pattern = re.compile(word)
      match = pattern.search(text)
@@ -28,6 +32,11 @@ with open(os.path.join(__location__, file_name+".txt")) as file:
     original_dfa = { "s_state" : set() , "n_states" : set() , "e_states" : set()}
     minimized_dfa = { "s_state" : set() , "n_states" : set() , "e_states" : set()}
 
+    alphabet = set()
+
+    transitions = {}
+
+
     print(contents + '\n')
     print(state_span)
 
@@ -49,9 +58,51 @@ with open(os.path.join(__location__, file_name+".txt")) as file:
             original_dfa["e_states"].add(row[1:-1])
             continue
         else:
-            original_dfa["n_states"].add(row[0:-1])
+            original_dfa["n_states"].add(row[:-1])
         
         row_counter+=1
+    
+    file.seek(alphabet_span[1]+8)          #begin reading from Alfabeto\n onwards
+    row_counter, valid_row_counter = 0 , 0
 
+    for row in file:
+        if row == "Transiciones\n":
+            break
+        if row == r'^\s+\n$':       #if row is empty, continue to the next iteration
+            continue
+        
+        alphabet.add(row[:-1])
+
+        row_counter+=1
+    
+
+    file.seek(transition_span[1])          #begin reading from Alfabeto\n onwards
+    row_counter, valid_row_counter = 0 , 0
+
+    for row in file:
+        if row == r'^\s+\n$':       #if row is empty, continue to the next iteration
+            continue
+        
+        pattern = re.compile(transition)
+        match = pattern.search(row)
+        
+        if match:
+            transitions[frozenset([match.group(1),match.group(2)])] = set(match.group(3))
+
+        row_counter+=1
+
+
+    # minimization algorithm
+    
+
+
+    # print for testing
     for key in original_dfa:
-        print(original_dfa[key])
+        print(key, original_dfa[key])
+
+    print(alphabet)
+
+    for key in transitions:
+        print(key, transitions[key])
+
+    print(transitions[frozenset(['0','B'])])
